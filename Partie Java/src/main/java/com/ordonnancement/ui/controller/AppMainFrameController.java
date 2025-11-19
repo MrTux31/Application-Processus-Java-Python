1f16d712458bc5323c7df8b70b0d75eaee57b252
MrTux31 <quentstarwars@gmail.com>
2025-11-19 11:51:38 +0100

    Affiche exception corrigé

--- Partie Java/src/main/java/com/ordonnancement/ui/controller/AppMainFrameController.java
+++ Partie Java/src/main/java/com/ordonnancement/ui/controller/AppMainFrameController.java
@@ -1,6 +1,5 @@
 package com.ordonnancement.ui.controller;
 
-
 import com.ordonnancement.ui.Alert.AlertUtils;
 
 import javafx.fxml.FXML;
@@ -11,16 +10,18 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 /**
  * Contrôleur principal de l'application.
- * Gère la fenêtre principale et la navigation entre les différentes fonctionnalités.
+ * Gère la fenêtre principale et la navigation entre les différentes
+ * fonctionnalités.
  */
 public class AppMainFrameController {
 
-    //Charger éléments de la vue FXML
-    @FXML 
-    StackPane mainContentPane; //Conteneur principal
-    @FXML 
+    // Charger éléments de la vue FXML
+    @FXML
+    StackPane mainContentPane; // Conteneur principal
+    @FXML
     MenuBar menuBar;
     @FXML
     Menu configMenu;
@@ -35,28 +36,45 @@ public class AppMainFrameController {
     @FXML
     MenuItem ganttProcessMenu;
     //////
-    
-    //Controleurs 
+
+    // Controleurs
     GanttProcessorController ganttProcessorController;
 
     /**
      * Affiche le gantt par cpu
+     * 
      * @author ROMA Quentin
      */
     @FXML
     private void doAfficherGanttCPU() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/GanttProcessorView.fxml")); // Préparer le chargement du FXML
-            BorderPane ganttProcessor = loader.load(); //Charger le fxml
-            mainContentPane.getChildren().setAll(ganttProcessor);  //Mettre le border pane dans le stackpane
-            ganttProcessorController = loader.getController(); //Récupérer le controleur de la vue
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/GanttProcessorView.fxml")); // Préparer le
+                                                                                                         // chargement
+                                                                                                         // du FXML
+            BorderPane ganttProcessor = loader.load(); // Charger le fxml
+            mainContentPane.getChildren().setAll(ganttProcessor); // Mettre le border pane dans le stackpane
+            ganttProcessorController = loader.getController(); // Récupérer le controleur de la vue
 
         } catch (Exception e) {
-            AlertUtils.showError("Erreur", "Impossible d'ouvrir le gantt par CPU :\n" + e.getMessage(),(Stage) mainContentPane.getScene().getWindow());
-            e.printStackTrace();
+
+            Throwable root = e;
+            while (root.getCause() != null) {
+                root = root.getCause();
+            }
+
+            // Récupérer le message réel de l'exception racine
+            String cleanMessage = root.getMessage();
+            if (cleanMessage == null || cleanMessage.isEmpty()) {
+                cleanMessage = root.toString(); // fallback si pas de message
+            }
+
+            AlertUtils.showError(
+                    "Erreur",
+                    "Erreur affichange Gantt CPU :\n" + cleanMessage,
+                    (Stage) mainContentPane.getScene().getWindow());
+
         }
+
     }
-
-
 
 }
